d4d49edb842fdfd93a7af14089d2103d67d0e8b0
Rewrisek <rewrisek@gmail.com>
2026-06-10 02:02:33 +0200

    revert of scale

--- src/components/ArchitectureModel.jsx
+++ src/components/ArchitectureModel.jsx
@@ -44,7 +44,7 @@ function Building() {
         box.getSize(size);
         box.getCenter(center);
 
-        const scale = 3.5;
+        const scale = 4;
         scene.scale.setScalar(scale);
         scene.position.set(-center.x * scale, -box.min.y * scale, -center.z * scale);
 
